ceeac703392e4227c9fc707ec4b19325f75b7f5d
Winston <winstonwut@live.com>
2015-05-12 21:31:09 -0400

    Initial Commit

---
+++ AnimeDesktop/Sources/AnimeMobile.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Application = System.Windows.Application;
+
+namespace AnimeDesktop.Sources
+{
+	class AnimeMobile : AnimeSource
+	{
+		public override void Initialize()
+		{
+			Animes = GetAnimes().ToList();
+		}
+
+		public override IEnumerable<Anime> GetAnimes()
+		{
+			const string link = "http://www.animemobile.com/service/mobile2.php?&all_list=true";
+			var data = new WebClient().DownloadString(link);
+			var json = JsonConvert.DeserializeObject<JObject>(data)["list"];
+			return from members in json.Children() from values in members.Children() from anime in values select new Anime
+			{
+				Id = int.Parse(anime["ID"].ToString()),
+				Title = anime["Title"].ToString(),
+				Seo = anime["Seo"].ToString(),
+				Letter = anime["Letter"].ToString(),
+				Language = int.Parse(anime["Language"].ToString()),
+				Status = int.Parse(anime["Status"].ToString()),
+				Alternatives = anime["Alternatives"].ToString().Split(new [] {','}, StringSplitOptions.RemoveEmptyEntries),
+				Categories = anime["Categories"].ToString().Split(new[] {' '}, StringSplitOptions.RemoveEmptyEntries)
+			};
+		}
+
+		public override AnimeInfo GetAnimeData(Anime anime)
+		{
+			var link = "http://www.animemobile.com/service/mobile2.php?&anime_id=" + anime.Id;
+			var data = new WebClient().DownloadString(link);
+			return JsonConvert.DeserializeObject<AnimeInfo>(data);
+		}
+
+		public override string[] GetEpisodeLinks(int episodeId)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
